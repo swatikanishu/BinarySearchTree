8941690669e6cb76f01b953da2b488e20e6662d6
swatikanishu <nishuswatika@gmail.com>
2022-07-03 12:08:50 +0530

    Added: Welcome message is added

---
+++ src/com/bridgelabz/MainBinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MainBinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("welcome to Binary Search Tree" +
+                " program");
+    }
+}
