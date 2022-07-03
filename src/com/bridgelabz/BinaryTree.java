2e69ebaabd2058f9dcfb1b1cbd070a883869e91b
swatikanishu <nishuswatika@gmail.com>
2022-07-03 12:24:12 +0530

    Added:ability to add value in BST from figure

--- src/com/bridgelabz/BinaryTree.java
+++ src/com/bridgelabz/BinaryTree.java
@@ -1,48 +1,55 @@
 package com.bridgelabz;
 
-    class Node {
-        int data;
-        Node left;
-        Node right;
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+}//  in this class all method is non static so create object for this class in main method
+public class BinaryTree {
+    // method name as insert
+    public Node insert(Node root, int val) {
+        if (root == null) {
+            return craeteNewNode(val);
+        }
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else if (val > root.data) {
+            root.right = insert(root.right, val);
+        }
+        return root;
 
     }
-    //  in this class all method is non static so create object for this class in main method
-    public class BinaryTree {
-        //method to insert root and value
-        public Node insert(Node root, int val) {
 
-            if (root == null) {
-                return craeteNewNode(val);
-            }
-            if (val < root.data) {
-                root.left = insert(root.left, val);
-
-            } else if (val > root.data) {
-                root.right = insert(root.right, val);
-            }
-            return root;
-
-        }
-        //method name as createNewNode
-        public Node craeteNewNode(int k) {
-            Node bst = new Node();
-            bst.data = k;
-            bst.left = null;
-            bst.right = null;
-            return bst;
-        }
-
-        // create a method name as print , they are printing data
-        public void print(Node root) {
-
-            if (root == null) {
-                return;
-            }
-            print(root.left);
-            System.out.println(root.data);
-
-            //null right side
-            print(root.right);
-        }
+    // method name as createNewNode
+    public Node craeteNewNode(int k) {
+        Node bst = new Node();
+        bst.data = k;
+        bst.left = null;
+        bst.right = null;
+        return bst;
     }
 
+    // method to print
+    public void print(Node root) {
+        if (root == null) {
+            return;
+        }
+        print(root.left);
+
+        System.out.println(root.data);
+        print(root.right);
+    }
+
+    //Size method For checking all nodes are added or Not
+    public int size(Node node) {
+        /*
+         *  if condition is true then node is empty if condition is false then
+         * else condition is worked
+         */
+        if (node == null)
+            return 0;
+        else
+            return(size(node.left) + 1 + size(node.right));
+    }
+}
--- src/com/bridgelabz/MainBinarySearchTree.java
+++ src/com/bridgelabz/MainBinarySearchTree.java
@@ -14,6 +14,18 @@ public class MainBinarySearchTree {
 
         // 30->56->70->80
         root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
+
+        bst.print(root);
 
         bst.print(root);
 
